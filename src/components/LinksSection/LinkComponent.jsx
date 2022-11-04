731e11b2d51e9bf00c10045cfbf056935b1361e2
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-11-04 15:24:04 +0100

    feat:created a new page for contact

--- src/components/LinksSection/LinkComponent.jsx
+++ src/components/LinksSection/LinkComponent.jsx
@@ -1,11 +1,13 @@
-
 import "./link_component.css"
+import { Link } from "react-router-dom";
 
-function LinkComponent({name,id,address,title}) {
+function LinkComponent({name,id,address,title,type}) {
 
   return (
     <div className="link__component">
+      {type === "a" ? (
         <a id={`${id}`} title={`${title ? title : ""}`} href={`${address}`} target="__blanck">{name}</a>
+      ):(<Link id={`${id}`} title={`${title ? title : ""}`} to={`${address}`}>{name}</Link>)}  
     </div>
   )
 }
--- src/components/LinksSection/Links.jsx
+++ src/components/LinksSection/Links.jsx
@@ -5,18 +5,19 @@ import Slack from "../../assets/slack.png"
 
 function Links({slackName}) {
     const links = [
-        {name:"Twitter Link",id:"",address:"https://twitter.com/ibkay998",title:""},
-        {name:"Zuri Team",id:"btn__zuri",address:"https://training.zuri.team/",title:""},
-        {name:"Zuri Books",id:"books",address:"http://books.zuri.team",title:"this is where you find books about design and coding"},
-        {name:"Python Books",id:"book__python",address:`https://books.zuri.team/python-for-beginners?ref_id=${slackName}`,title:""},
-        {name:"Background Check for Coders",id:"pitch",address:"https://background.zuri.team",title:""},
-        {name:"Design Books",id:"book__design",address:"https://books.zuri.team/design-rules",title:""},
+        {name:"Twitter Link",id:"",address:"https://twitter.com/ibkay998",title:"",type:"a"},
+        {name:"Zuri Team",id:"btn__zuri",address:"https://training.zuri.team/",title:"",type:"a"},
+        {name:"Zuri Books",id:"books",address:"http://books.zuri.team",title:"this is where you find books about design and coding",type:"a"},
+        {name:"Python Books",id:"book__python",address:`https://books.zuri.team/python-for-beginners?ref_id=${slackName}`,title:"",type:"a"},
+        {name:"Background Check for Coders",id:"pitch",address:"https://background.zuri.team",title:"",type:"a"},
+        {name:"Design Books",id:"book__design",address:"https://books.zuri.team/design-rules",title:"",type:"a"},
+        {name:"Contact",id:"contact",address:"/contact",title:"",type:"link"},
 ]
 
   return (
     <div className="links">
         {links.map((link)=>(
-            <LinkComponent name={link.name} id={link.id} address={link.address} title={link.title} />
+            <LinkComponent name={link.name} id={link.id} address={link.address} title={link.title} type={link.type} />
         ))}
         <div className="social__links">
             <a href="https://slack.com/U029AMRJN2C"><img src={Slack} alt="slack"/></a>
